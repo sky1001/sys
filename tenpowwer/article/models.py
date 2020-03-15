45bec1bcf0229f3cf9c9fab58208e5097e5e2f7e
xwp <xwp_fullstack@163.com>
2020-03-15 14:49:21 +0800

    建立问题和文章的模型类修改

--- tenpowwer/article/models.py
+++ tenpowwer/article/models.py
@@ -21,16 +21,16 @@ class Article(models.Model):
         verbose_name = "文章"
         verbose_name_plural = verbose_name
         ordering = ['-createtime']
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 class Channel(models.Model):
     name = models.CharField(max_length=100, null=True, default=None, verbose_name="频道名称")
     class Meta:
         db_table = "tb_channel"
         verbose_name = "频道"
         verbose_name_plural = verbose_name
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 class Comment(models.Model):
     content = models.CharField(max_length=1000,default='', verbose_name='回答内 容')
     createtime = models.DateTimeField(auto_now_add=True, null=True, verbose_name="创建日期")
@@ -39,10 +39,11 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments", default=None,verbose_name="问题ID")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", default=None, verbose_name="用户ID")
 
-    class Meta: db_table = "article_comment"
+    class Meta:
+        db_table = "article_comment"
 
-    verbose_name = "评论"
-    verbose_name_plural = verbose_name
-    ordering = ['-createtime']  # 指明默认排序
+        verbose_name = "评论"
+        verbose_name_plural = verbose_name
+        ordering = ['-createtime']  # 指明默认排序
     def __str__(self):
         return self.content
