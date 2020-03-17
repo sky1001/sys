401af97b88b0b16f7172683b2011c73b77d3fd0a
QuantZzz <quantzzz@163.com>
2020-03-17 10:15:09 +0800

    招聘B类接口  收藏/取消收藏企业   企业访问量   收藏/取消收藏职位   职位访问量    详情页(企业/职位) 问题点：1.  更多职位页面  默认选择城市   屏蔽了功能：查询所有职位；仅根据关键词查询职位 2. 招聘首页的热门企业  没有链接跳转到 企业详情页面

--- tenpowwer/recruit/views.py
+++ tenpowwer/recruit/views.py
@@ -35,6 +35,46 @@ class EnterpriseViewSet(ModelViewSet):
         serializer = self.get_serializer(instance=hot_enterprises, many=True)
         return Response(serializer.data)
 
+    # 公司信息访问量
+    @action(methods=['put'], detail=True)
+    def visit(self, request, pk):
+        enterprise = self.get_object()
+        enterprise.visits += 1
+        enterprise.save()
+        return Response({'success': True, 'message': '更新成功'})
+
+    # 收藏企业
+    @action(methods=['post'], detail=True)
+    def collect(self, request, pk):
+        try:
+            user = request.user
+        except Exception:
+            user = None
+        if user is not None and user.is_authenticated:
+            enterprise = self.get_object()
+            enterprise.users.add(user)
+            enterprise.save()
+            return Response({'success': True, 'message': '收藏成功'})
+        else:
+            return Response({'success': False, 'message': '未登录'}, status=400)
+            # 收藏企业 enterprise/{pk}/collect/
+
+    # 取消收藏企业
+    @action(methods=['post'], detail=True)
+    def cancelcollect(self, request, pk):
+        try:
+            user = request.user
+        except Exception:
+            user = None
+        if user is not None and user.is_authenticated:
+            enterprise = self.get_object()
+            enterprise.users.remove(user)
+            enterprise.save()
+            return Response({'success': True, 'message': '取消收藏成功'})
+        else:
+            return Response({'success': False, 'message': '未登录'}, status=400)
+
+
 class RecruitViewSet(ModelViewSet):
     queryset = Recruit.objects.filter(state="1").order_by("-createtime")
 
@@ -59,4 +99,73 @@ class RecruitViewSet(ModelViewSet):
         serializer = self.get_serializer(instance=jobs, many=True)
         return Response(serializer.data)
 
+    # 根据城市名称和关键字搜索职位
+    @action(methods=["POST"], detail=False, url_path="search/city/keyword")
+    def search_job(self, request):
+        cityname = request.data.get('cityname')
+        keyword = request.data.get('keyword')
+        jobs = self.get_queryset()
+        ret_jobs = []
+        # 没选城市  也没给关键词  查所有职位
+        if not cityname and not keyword:
+            ret_jobs = jobs
+        # 有城市  没有关键词  查该城市的所有职位
+        elif cityname and not keyword:
+            for job in jobs:
+                if job.city == cityname:
+                    ret_jobs.append(job)
+        #  没城市  但有关键词  查包含关键词的所有职位
+        elif not cityname and keyword:
+            for job in jobs:
+                if job.jobname.lower().find(keyword.lower()) != -1:
+                    ret_jobs.append(job)
+        # 既有城市 又有关键词  查询指定职位
+        else:
+            for job in jobs:
+                if job.city == cityname and job.jobname.lower().find(keyword.lower()) != -1:
+                    ret_jobs.append(job)
+
+        serializer = self.get_serializer(instance=ret_jobs, many=True)
+        return Response(serializer.data)
+
+    # 招聘信息访问量
+    @action(methods=['put'], detail=True)
+    def visit(self, request, pk):
+        recruit = self.get_object()
+        recruit.visits += 1
+        recruit.save()
+        return Response({'success': True, 'message': '更新成功'})
+
+    # 收藏招聘信息
+    @action(methods=['post'], detail=True)
+    def collect(self, request, pk):
+        try:
+            user = request.user
+        except Exception:
+            user = None
+
+        if user is not None and user.is_authenticated:
+            recruit = self.get_object()
+            recruit.users.add(user)
+            recruit.save()
+            return Response({'success':True,'message':'收藏成功'})
+        else:
+            return Response({'success':False,'message':'未登录'}, status=400)
+
+
+    # 取消收藏招聘信息
+    @action(methods=['post'], detail=True)
+    def cancelcollect(self, request, pk):
+        try:
+            user = request.user
+        except Exception:
+            user = None
+
+        if user is not None and user.is_authenticated:
+            recruit = self.get_object()
+            recruit.users.remove(user)
+            recruit.save()
+            return Response({'success': True, 'message': '取消收藏成功'})
+        else:
+            return Response({'success': False, 'message': '未登录'}, status=400)
 
